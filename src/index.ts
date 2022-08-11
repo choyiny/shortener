6fa567674b5a6b48f5be7074b6e866b04f1da489
Cho Yin Yong <choyiny@gmail.com>
2022-08-11 19:58:02 -0400

    chore: run prettier and change HOST_URL

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,31 @@ const router = Router();
 
 export interface Env {
   SHORTENER_KV: KVNamespace;
-	HOST_URL: string;
+  HOST_URL: string;
 }
 
 router.post("/", async (request: Request, env: Env) => {
-  // TODO: Post request to shorten a URL
+  const json = await request.json();
+  const longLink = json?.url;
+  // generate a random slug - this is not cryptographically random but works in our case
+  let slug = btoa(Math.random() + "").slice(0, 9);
+  // generate a new slug if the previous one is already existing
+  let existing = await env.SHORTENER_KV.get(slug);
+  while (existing) {
+    slug = btoa(Math.random() + "").slice(0, 9);
+    existing = await env.SHORTENER_KV.get(slug);
+  }
+  // generate shortened URL and return to user
+  await env.SHORTENER_KV.put(slug, longLink);
+  return new Response(JSON.stringify({ url: `${env.HOST_URL}/${slug}` }), {
+    headers: { "content-type": "application/json" },
+  });
 });
 
 router.get("/:slug", async (request: Request, env: Env) => {
-  // TODO: redirect shortened URL to application URL
+  const slug = request.params.slug;
+  const redirectTo = await env.SHORTENER_KV.get(slug);
+  return Response.redirect(redirectTo, 302);
 });
 
 export default {
